16f9a1e359e8a4082cef2e8815db35564bdaf94b
Gustavo <gustavo.moraes@yaman.com.br>
2018-11-26 14:27:22 -0200

    implantacao de cenário positivo e negativo

--- features/specs/testa_livros.feature
+++ features/specs/testa_livros.feature
@@ -2,7 +2,12 @@
 
 Funcionalidade: Verificar existência de livro em três livrarias
 
-Cenario: Acessar o site do Submarino e depois em mais duas livrarias e comparar o mesmo livro, buscar nome e verificar se o título é do mesmo autor
-Dado Acessar primeiro livro do submarino, guardar nome e autor
-Quando Verificar a existência do livro em mais duas livrarias
-Então Validar título e se o autor é o mesmo
+Esquema do Cenario: Acessar o site do Submarino e depois em mais duas livrarias e comparar o mesmo livro, buscar nome e verificar se o título é do mesmo autor
+Dado Acessar primeiro livro do submarino, guardar nome e autor 
+Quando Verificar a existência do livro em mais duas livrarias "<tipo>"
+Então Validar título e se o autor é o mesmo "<tipo>"
+
+Exemplos:
+  | tipo     |
+  | positivo |
+  | negativo |
--- features/step_definitions/testa_livros.rb
+++ features/step_definitions/testa_livros.rb
@@ -6,19 +6,24 @@ Dado("Acessar primeiro livro do submarino, guardar nome e autor") do
     @resultado_autor = @home.autor
 end
 
-Quando("Verificar a existência do livro em mais duas livrarias") do
+Quando("Verificar a existência do livro em mais duas livrarias {string}") do |tipo|
     @opcao1 = Pagina_Inicial_Saraiva.new
     @opcao1.load
     @opcao1.busca @resultado
-    @saraiva_autor = @opcao1.resultado_busca
+    @saraiva_autor = tipo.eql?('positivo') ? @opcao1.resultado_busca : nil
 
     @opcao2 = Pagina_Inicial_Cultura.new
     @opcao2.load
     @opcao2.busca @resultado
-    @cultura_autor = @opcao2.resultado_busca
+    @cultura_autor = tipo.eql?('positivo') ? @opcao2.resultado_busca : nil
 end
 
-Então("Validar título e se o autor é o mesmo") do
+Então("Validar título e se o autor é o mesmo {string}") do |tipo|
+    if tipo.eql?('positivo')
     expect(@saraiva_autor).to include @resultado_autor[8..15]
     expect(@cultura_autor).to include @resultado_autor[8..15]
+    else
+        expect(@saraiva_autor).to eql nil
+        expect(@cultura_autor).to eql nil
+    end
 end
